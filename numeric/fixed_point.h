0be6ef7ecdcd1890c066727c2a3bde3790d0ea55
BartolomeyKant <notevill@gmail.com>
2025-05-26 15:43:27 +0500

    add numeric

---
+++ numeric/fixed_point.h
@@ -0,0 +1,301 @@
+/*
+ * Copyright 2025 Aethernet Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef NUMERIC_FIXED_POINT_H_
+#define NUMERIC_FIXED_POINT_H_
+
+#include <limits>
+#include <cstdint>
+#include <algorithm>
+#include <type_traits>
+
+#include "third_party/gcem/include/gcem.hpp"
+
+namespace ae {
+// Compile-time fixed point number representation.
+// A number is stored as floating point value multiplied by scale.
+// A point position meaning:
+//   0 - range represented [0..1). For example, for std::uint8_t 1.0 would be
+//   encoded as 256,
+//       so the maximum value encoded is 1.0 - 1/256. Rounding is performed as
+//       truncating. min value is 1/256
+//   1 - a single bit represents the integer part of the number. range is
+//   [0..2). min = 1/127 8 - float is just truncated to integer [0..255], min =
+//   1 9 - floating point is multiplied by 0.5 and truncated. range = [0..510],
+//   min = 2 -1 - range = [0..0.5), min value is 1/510
+
+template <typename T, T max_value, int int_bits>
+struct FixedPoint {
+  using Type = T;
+  static constexpr Type kMaxValue = max_value;
+  static constexpr int kIntBits = int_bits;
+  static constexpr int kTotalBits = sizeof(Type) * 8;
+
+  static constexpr double kMinValue =
+      kIntBits < kTotalBits
+          ? 1.0 / gcem::pow(2.0, static_cast<double>(kTotalBits - kIntBits))
+          : gcem::pow(2.0, static_cast<double>(kIntBits - kTotalBits));
+  static constexpr double kMinValueInv = 1 / kMinValue;
+  Type value_;
+
+  constexpr FixedPoint() = default;
+
+  constexpr explicit FixedPoint(Type value) : value_(value) {}
+
+  template <typename ST, ST mv, int nb>
+  constexpr FixedPoint(const FixedPoint<ST, mv, nb>& f) {
+    value_ = Cast(f).value_;
+  }
+
+  constexpr FixedPoint(double value) : value_(Value(value)) {}
+
+  constexpr operator double() const { return value_ * kMinValue; }
+
+  auto& operator=(double value) {
+    value_ = Value(value);
+    return *this;
+  }
+
+  template <typename ST, ST mv, int nb>
+  auto& operator=(const FixedPoint<ST, mv, nb>& s) {
+    value_ = Cast(s).value_;
+    return *this;
+  }
+
+  // TODO: force compile-time only.
+  static constexpr Type Value(double f) {
+    return static_cast<Type>(f * kMinValueInv);
+  }
+
+  template <typename ST, ST mv, int nb>
+  constexpr FixedPoint<Type, kMaxValue, kIntBits>& Cast(
+      const FixedPoint<ST, mv, nb>& f) {
+    static_assert(kIntBits >= nb, "Assignment may cause overflow");
+
+    using ThisType = FixedPoint<Type, kMaxValue, kIntBits>;
+    return ThisType(f.value_);
+  }
+};
+
+template <typename Type>
+constexpr int CalculateIntegerBits(double v) {
+  constexpr double rr = gcem::pow(2.0, (8.0 * sizeof(Type)));
+  constexpr double c = 1.0 - 1.0 / rr;
+  if (v > c) {
+    return static_cast<int>(gcem::ceil(gcem::log2(v))) +
+           (v > c * (1ul << static_cast<int>(gcem::ceil(gcem::log2(v)))) ? 1
+                                                                         : 0);
+  } else if (v >= c / 2) {
+    return 0;
+  } else {
+    return -static_cast<int>(gcem::floor(gcem::log2(1 / v))) -
+           (v > c / (1ul << static_cast<int>(gcem::floor(gcem::log2(1 / v))))
+                ? 1
+                : 0);
+  }
+}
+
+template <typename T>
+constexpr bool CompareDoubles(const double v1, const double v2) {
+  return gcem::abs(v1 - v2) / gcem::max(v1, v2) <
+         1.0 / gcem::pow(2.0, static_cast<double>(8 * sizeof(T)));
+}
+
+template <int n>
+struct TypeSelector1 {
+  using type = std::conditional_t<
+      (n > 32), std::uint64_t,
+      std::conditional_t<
+          (n > 16), std::uint32_t,
+          std::conditional_t<(n > 8), std::uint16_t, std::uint8_t>>>;
+};
+
+template <typename T, int n>
+struct TypeSelector {
+  using type =
+      typename TypeSelector1<(n < static_cast<int>(sizeof(T) * 8)
+                                  ? static_cast<int>(sizeof(T) * 8) - n
+                                  : n - static_cast<int>(sizeof(T) * 8))>::type;
+};
+
+template <typename R, typename F1, typename F2>
+constexpr auto Add(const F1& v1, const F2& v2) {
+  constexpr double max = static_cast<double>(F1::kMaxValue) * F1::kMinValue +
+                         static_cast<double>(F2::kMaxValue) * F2::kMinValue;
+  constexpr auto ib = CalculateIntegerBits<R>(max);
+  constexpr auto i = FixedPoint<R, 0, ib>::Value(max);
+  double r = static_cast<double>(v1) + static_cast<double>(v2);
+  using RT = FixedPoint<R, i, ib>;
+  return RT(RT::Value(r));
+}
+
+template <typename T1, T1 max_value1, int ib1, typename T2, T2 max_value2,
+          int ib2>
+constexpr auto operator+(const FixedPoint<T1, max_value1, ib1>& v1,
+                         const FixedPoint<T2, max_value2, ib2>& v2) {
+  return Add<typename TypeSelector1<gcem::max(
+      FixedPoint<T1, max_value1, ib1>::kTotalBits,
+      FixedPoint<T2, max_value2, ib2>::kTotalBits)>::type>(v1, v2);
+}
+
+template <typename R, typename F1, typename F2>
+constexpr auto Sub(const F1& v1, const F2& v2) {
+  double r = static_cast<double>(v1) - static_cast<double>(v2);
+  using RT = FixedPoint<R, F1::kMaxValue, F1::kIntBits>;
+  return RT(RT::Value(r));
+}
+
+template <typename T1, T1 max_value1, int ib1, typename T2, T2 max_value2,
+          int ib2>
+constexpr auto operator-(const FixedPoint<T1, max_value1, ib1>& v1,
+                         const FixedPoint<T2, max_value2, ib2>& v2) {
+  return Sub<typename FixedPoint<T1, max_value1, ib1>::Type>(v1, v2);
+}
+
+template <typename R, typename F1, typename F2>
+constexpr auto Mul(const F1& v1, const F2& v2) {
+  constexpr double max = static_cast<double>(F1::kMaxValue) * F1::kMinValue *
+                         static_cast<double>(F2::kMaxValue) * F2::kMinValue;
+  constexpr auto ib = CalculateIntegerBits<R>(max);
+  constexpr auto i = FixedPoint<R, 0, ib>::Value(max);
+  double r = static_cast<double>(v1) * static_cast<double>(v2);
+  using RT = FixedPoint<R, i, ib>;
+  return RT(RT::Value(r));
+}
+
+template <typename T1, T1 max_value1, int ib1, typename T2, T2 max_value2,
+          int ib2>
+constexpr auto operator*(const FixedPoint<T1, max_value1, ib1>& v1,
+                         const FixedPoint<T2, max_value2, ib2>& v2) {
+  return Mul<typename TypeSelector1<gcem::max(
+      FixedPoint<T1, max_value1, ib1>::kTotalBits,
+      FixedPoint<T2, max_value2, ib2>::kTotalBits)>::type>(v1, v2);
+}
+
+template <typename R, R min_value, typename F1, typename F2>
+constexpr auto Div(const F1& v1, const F2& v2) {
+  constexpr double max = static_cast<double>(F1::kMaxValue) * F1::kMinValue /
+                         static_cast<double>(min_value * F2::kMinValue);
+  constexpr auto ib = CalculateIntegerBits<R>(max);
+  constexpr auto i = FixedPoint<R, 0, ib>::Value(max);
+  double r = static_cast<double>(v1) / static_cast<double>(v2);
+  using RT = FixedPoint<R, i, ib>;
+  return RT(RT::Value(r));
+}
+
+template <typename T1, T1 max_value1, int ib1, typename T2, T2 max_value2,
+          int ib2>
+constexpr auto operator/(const FixedPoint<T1, max_value1, ib1>& v1,
+                         const FixedPoint<T2, max_value2, ib2>& v2) {
+  return Div<typename TypeSelector1<gcem::max(
+                 FixedPoint<T1, max_value1, ib1>::kTotalBits,
+                 FixedPoint<T2, max_value2, ib2>::kTotalBits)>::type,
+             FixedPoint<T2, max_value2, ib2>::Value(
+                 FixedPoint<T2, max_value2, ib2>::kMinValue)>(v1, v2);
+}
+
+template <typename Exp, typename T, T max_value, int int_bits, T base, T scale>
+struct Exponent : public FixedPoint<T, max_value, int_bits> {
+  using Fix = FixedPoint<T, max_value, int_bits>;
+
+  static constexpr auto kBase = base;
+  static constexpr auto kScale = scale;
+
+  // inherit all constructors from Fix
+  using Fix::Fix;
+
+  constexpr Exp Serialize() const {
+    constexpr double lb = 1.0 / gcem::log2(kBase * Fix::kMinValue);
+    constexpr double s = gcem::log2(1.0 / kScale) * lb;
+    double p = gcem::log2(Fix::value_) * lb + s;
+    return static_cast<Exp>(p);
+  }
+
+  void Deserialize(Exp stored) {
+    constexpr double b = kBase * Fix::kMinValue;
+    // convert stored to common type
+    double e = gcem::pow(b, static_cast<std::uint64_t>(stored)) * kScale;
+    Fix::value_ = static_cast<T>(e);
+  }
+};
+
+// Options for an exponent type definition
+template <typename T>
+struct ExponentOptions {
+  double base_;
+  int total_bits_;
+  int int_bits_;
+
+  constexpr ExponentOptions(double min, double max)
+      : base_{CalcBase(min, max)},
+        total_bits_{
+            static_cast<int>(gcem::ceil(gcem::log2(CalcRange(min, max))))},
+        int_bits_{static_cast<int>(gcem::ceil(gcem::log2(max)))} {}
+
+ private:
+  static constexpr auto CalcBase(double min, double max) {
+    double v_ = max / min;
+    double n_ = gcem::pow(2ul, sizeof(T) * 8) - 1;
+    return gcem::pow(v_, 1 / n_);
+  }
+
+  static constexpr auto CalcRange(double min, double max) {
+    double v_ = max / min;
+    double n_ = gcem::pow(2ul, sizeof(T) * 8) - 1;
+    double base = gcem::pow(v_, 1 / n_);
+    double step_ = min * base - min;
+    return v_ / step_;
+  }
+};
+
+template <typename T>
+constexpr ExponentOptions<T> make_exponent_options(double min, double max) {
+  return ExponentOptions<T>(min, max);
+}
+}  // namespace ae
+
+namespace std {
+template <typename T, T max_value, int position>
+class numeric_limits<ae::FixedPoint<T, max_value, position>> {
+ public:
+  static constexpr double lowest() { return 0.0; }
+  static constexpr double min() {
+    return ae::FixedPoint<T, max_value, position>::kMinValue;
+  }
+  static constexpr double max() {
+    return ae::FixedPoint<T, max_value, position>::kMaxValue;
+  }
+};
+}  // namespace std
+
+#define AE_FIXED(T, M)                                                   \
+  ae::FixedPoint<                                                        \
+      T, ae::FixedPoint<T, 0, ae::CalculateIntegerBits<T>(M)>::Value(M), \
+      ae::CalculateIntegerBits<T>(M)>
+
+#define _EXP_OPT_(T, MIN, MAX) ae::make_exponent_options<T>(MIN, MAX)
+
+#define _FIXED_OPT_(T, MIN, MAX) \
+  AE_FIXED(ae::TypeSelector1<_EXP_OPT_(T, MIN, MAX).total_bits_>::type, MAX)
+
+#define AE_EXPONENT(T, MIN, MAX)                                              \
+  ae::Exponent<T, _FIXED_OPT_(T, MIN, MAX)::Type,                             \
+               _FIXED_OPT_(T, MIN, MAX)::kMaxValue,                           \
+               _EXP_OPT_(T, MIN, MAX).int_bits_,                              \
+               _FIXED_OPT_(T, MIN, MAX)::Value(_EXP_OPT_(T, MIN, MAX).base_), \
+               _FIXED_OPT_(T, MIN, MAX)::Value(MIN)>
+
+#endif  // NUMERIC_FIXED_POINT_H_ */
---
+++ numeric/tiered_int.h
@@ -0,0 +1,368 @@
+/*
+ * Copyright 2025 Aethernet Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef NUMERIC_TIERED_INT_H_
+#define NUMERIC_TIERED_INT_H_
+
+#include <array>
+#include <tuple>
+#include <limits>
+#include <cstdint>
+#include <functional>
+#include <type_traits>
+
+namespace ae {
+
+namespace tiered_int_internal {
+using TierTypeList =
+    std::tuple<std::uint8_t, std::uint16_t, std::uint32_t, std::uint64_t>;
+
+template <typename T, typename TypeList, std::size_t... Is>
+constexpr auto TierIndexImpl(std::index_sequence<Is...> const &) {
+  constexpr auto arr =
+      std::array{std::is_same_v<T, std::tuple_element_t<Is, TypeList>>...};
+
+  std::size_t res = 0;
+  for (; res < arr.size(); ++res) {
+    if (arr[res]) {
+      break;
+    }
+  }
+  return res;
+}
+
+template <typename T, typename TypeList = TierTypeList>
+constexpr std::size_t TierIndex() {
+  return TierIndexImpl<T, TypeList>(
+      std::make_index_sequence<std::tuple_size_v<TypeList>>());
+}
+/**
+ * \brief Limit variable calculations for tiers.
+ */
+template <typename MaxStored, typename MinStored, MinStored MinMaxStoredValue>
+struct Limit {
+  static constexpr auto kRangeIndex = TierIndex<MaxStored>();
+  using PrevTierLimit =
+      Limit<std::tuple_element_t<kRangeIndex - 1, TierTypeList>, MinStored,
+            MinMaxStoredValue>;
+  using StoredType = MaxStored;
+
+  static constexpr auto kAbsoluteMaxValue =
+      std::numeric_limits<StoredType>::max();
+
+  static constexpr auto kP = static_cast<StoredType>(1)
+                             << (std::numeric_limits<StoredType>::digits >> 1);
+
+  static constexpr auto kMaxStored =
+      (static_cast<StoredType>(PrevTierLimit::kMaxStored)) -
+      PrevTierLimit::kBorrowCount +
+      (PrevTierLimit::kBorrowCount *
+       (static_cast<StoredType>(PrevTierLimit::kAbsoluteMaxValue) + 1));
+
+  static constexpr auto kBorrowCount = PrevTierLimit::kP;
+
+  static constexpr auto kUpper =
+      kMaxStored - static_cast<StoredType>(PrevTierLimit::kP);
+};
+
+/**
+ * \brief Specialization for the first tier
+ */
+template <typename Stored, Stored MaxStoredValue>
+struct Limit<Stored, Stored, MaxStoredValue> {
+  using StoredType = Stored;
+  static constexpr auto kRangeIndex = TierIndex<StoredType>();
+  static constexpr auto kAbsoluteMaxValue =
+      std::numeric_limits<StoredType>::max();
+  static constexpr auto kBorrowCount =
+      std::numeric_limits<StoredType>::max() - MaxStoredValue;
+  static constexpr std::uint64_t kMaxStored = kAbsoluteMaxValue + 1;
+  static constexpr auto kUpper =
+      std::numeric_limits<StoredType>::max() - kBorrowCount + 1;
+  static constexpr auto kP = static_cast<Stored>(1)
+                             << (std::numeric_limits<Stored>::digits >> 1);
+};
+}  // namespace tiered_int_internal
+
+enum class TierDeserializeRes : std::uint8_t {
+  kNo,        // < not read
+  kFinished,  // < read whole value
+  kNext,      // < read only part of value
+};
+
+/**
+ * \brief Variable length integer value.
+ * All values divided on tiers and stored in a variable length format, e.g.
+ * [0..250] as std::uint8_t, [251..1514] as std::uint16_t, [1515..1049834] as
+ * std::uint32_t. The algorithm is optimized to store as more values as possible
+ * in the smallest tier and quite enough in the others. In contrast to other
+ * algorithms it doesn't reserve special bits to indicate that there are more
+ * tiers to read and this allows to more flexible settings how many values
+ * stored in tiers.
+ * Tier calculation is not obvious and depends on max value
+ * stored as min type (MinMaxStoredValue). For MinStoredType tier always would
+ * be a [0 - MinMaxStoredValue]. To calculate other tiers a few special
+ * variables should be calculated:
+ * - kP - is always 2^(half of digits in tier's type) (? TODO: explain why ?)
+ * - kBorrowCount - it is a count of values that next tier could borrow from
+ * current tier. It is calculated as (type's max value) -  MinMaxStoredValue
+ * for first tier and as (kP from previous tier) for others.
+ * - kMaxStored - is a max count of values that can be stored in current tier.
+ * For first tier it is always (type's max value + 1). For other tiers it is
+ * calculated as (prev tier kMaxStored) + (prev tier kBorrowCount) * (prev
+ * tier's type max value + 1).
+ * - kUpper - is a post max value in a current tier. For the first tier this
+ * calculated as type's max value - kBorrowCount + 1. For other tiers it is
+ * kMaxStored - (prev tier kP).
+ * So tier would be [pre tier's kUpper..kUpper).
+ * All variables calculations are made in compile time by Limit type \see Limit.
+ *
+ * Serialize algorithm:
+ *  - V is a currently stored value, and it can be divided in to two words: low
+ * and high.
+ *  - PrevUpper is a kUpper value for previous tier.
+ * Algorithm starts from the highest tier and goes down to the first - the
+ * minimum one.
+ * void Serialize(V) { if (V >= PrevUpper) { V -= PrevUpper;
+ *     high_part = V.high + PrevUpper;
+ *     Save(high_part);
+ *     write(V.low);
+ *   }
+ * }
+ * and for first tier:
+ * void Serialize(V) {
+ *   write(V);
+ * }
+ *
+ * Deserialize algorithm:
+ * V is a value to that we read
+ * Algorithm starts from the first tier and goes up to the highest one.
+ * Res Deserialize(V) {
+ *    res = Deserialize(V.high);
+ *    if (res is finished) {
+ *      V = V.high;
+ *    }
+ *    else if (res is next)
+ *   {
+ *     read(V.low);
+ *     V = V + PrevUpper;
+ *     if (V >= kUpper) {
+ *        V = V - kUpper;
+ *        return next;
+ *     }
+ *     return finished;
+ *   }
+ * }
+ * and for first tier:
+ * Res Deserialize(V) {
+ *   read(V);
+ *   if (V >= kUpper) {
+ *     V = V - kUpper;
+ *     return next;
+ *   }
+ *   return finished;
+ * }
+ *
+ * \tparam MaxStoredType - max value type that can be stored
+ * \tparam MinStoredType - min value type that can be stored
+ * \tparam MinMaxStoredValue - max value that can be saved in a
+ * sizeof(MinStoredType)
+ */
+// TODO: signed types are not supported
+
+template <typename MaxStoredType, typename MinStoredType,
+          MinStoredType MinMaxStoredValue>
+struct TieredInt {
+  using LimitType = tiered_int_internal::Limit<MaxStoredType, MinStoredType,
+                                               MinMaxStoredValue>;
+  using ValueType = typename LimitType::StoredType;
+  using PrevType = typename LimitType::PrevTierLimit::StoredType;
+  using PrevPacked = TieredInt<PrevType, MinStoredType, MinMaxStoredValue>;
+
+  static constexpr ValueType kUpper = LimitType::kUpper;
+
+#pragma pack(push, 1)
+  union Storage {
+    ValueType value;
+    struct {
+      PrevType low;
+      PrevType high;
+    } st;
+  };
+#pragma pack(pop)
+
+  template <typename Tother>
+  TieredInt(Tother v) {
+    value.value = static_cast<ValueType>(v);
+  }
+  TieredInt() = default;
+
+  operator ValueType &() noexcept { return value.value; }
+  operator ValueType const &() const noexcept { return value.value; }
+
+  template <typename TStream>
+  TierDeserializeRes Deserialize(TStream &is) {
+    constexpr auto prev_upper = PrevPacked::kUpper;
+
+    auto high = PrevPacked{};
+    auto res = high.Deserialize(is);
+    switch (res) {
+      case TierDeserializeRes::kNo:
+        return res;
+      case TierDeserializeRes::kFinished: {
+        value.value = static_cast<ValueType>(
+            static_cast<typename PrevPacked::ValueType>(high));
+        return TierDeserializeRes::kFinished;
+      }
+      case TierDeserializeRes::kNext: {
+        value.st.high = static_cast<typename PrevPacked::ValueType>(high);
+        is >> value.st.low;
+        value.value += prev_upper;
+        if (value.value >= kUpper) {
+          value.value -= kUpper;
+          return TierDeserializeRes::kNext;
+        }
+        break;
+      }
+    }
+    return TierDeserializeRes::kFinished;
+  }
+
+  template <typename TStream>
+  void Serialize(TStream &os) const {
+    constexpr auto prev_upper = PrevPacked::kUpper;
+
+    if (value.value >= prev_upper) {
+      auto modified = value;
+      modified.value -= prev_upper;
+      PrevPacked{modified.st.high + prev_upper}.Serialize(os);
+      os << modified.st.low;
+      return;
+    }
+    PrevPacked{value.st.low}.Serialize(os);
+  }
+
+  Storage value;
+};
+
+/**
+ * \brief Specialization for the first tier.
+ */
+template <typename StoredType, StoredType MaxValue>
+struct TieredInt<StoredType, StoredType, MaxValue> {
+  using LimitType =
+      tiered_int_internal::Limit<StoredType, StoredType, MaxValue>;
+  using ValueType = typename LimitType::StoredType;
+
+  static constexpr ValueType kUpper = LimitType::kUpper;
+
+  struct Storage {
+    ValueType value;
+  };
+
+  template <typename Tother>
+  TieredInt(Tother v) {
+    value.value = static_cast<ValueType>(v);
+  }
+  TieredInt() = default;
+
+  operator ValueType &() noexcept { return value.value; }
+  operator ValueType const &() const noexcept { return value.value; }
+
+  template <typename TStream>
+  TierDeserializeRes Deserialize(TStream &is) {
+    is >> value.value;
+    if (value.value >= kUpper) {
+      value.value -= kUpper;
+      return TierDeserializeRes::kNext;
+    }
+    return TierDeserializeRes::kFinished;
+  }
+
+  template <typename TStream>
+  void Serialize(TStream &os) const {
+    os << value.value;
+  }
+
+  Storage value;
+};
+
+template <typename TStream, typename T, typename Min, Min MinMaxVal>
+TStream &operator<<(TStream &os, TieredInt<T, Min, MinMaxVal> const &v) {
+  v.Serialize(os);
+  return os;
+}
+
+template <typename TStream, typename T, typename Min, Min MinMaxVal>
+TStream &operator>>(TStream &is, TieredInt<T, Min, MinMaxVal> &v) {
+  v.Deserialize(is);
+  return is;
+}
+
+template <typename T1, typename Min1, Min1 MinMaxVal1, typename T2,
+          typename Min2, Min2 MinMaxVal2>
+int TieredIntCompare(TieredInt<T1, Min1, MinMaxVal1> const &left,
+                     TieredInt<T2, Min2, MinMaxVal2> const &right) {
+  if (left.value.value < right.value.value) {
+    return -1;
+  } else if (left.value.value > right.value.value) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+template <typename T1, typename Min1, Min1 MinMaxVal1, typename T2,
+          typename Min2, Min2 MinMaxVal2>
+static bool operator==(TieredInt<T1, Min1, MinMaxVal1> const &left,
+                       TieredInt<T2, Min2, MinMaxVal2> const &right) {
+  return TieredIntCompare(left, right) == 0;
+}
+
+template <typename T1, typename Min1, Min1 MinMaxVal1, typename T2,
+          typename Min2, Min2 MinMaxVal2>
+static bool operator<(TieredInt<T1, Min1, MinMaxVal1> const &left,
+                      TieredInt<T2, Min2, MinMaxVal2> const &right) {
+  return TieredIntCompare(left, right) < 0;
+}
+
+template <typename T1, typename Min1, Min1 MinMaxVal1, typename T2,
+          typename Min2, Min2 MinMaxVal2>
+static bool operator>(TieredInt<T1, Min1, MinMaxVal1> const &left,
+                      TieredInt<T2, Min2, MinMaxVal2> const &right) {
+  return TieredIntCompare(left, right) > 0;
+}
+
+}  // namespace ae
+
+namespace std {
+template <typename T, typename Min, Min MinMaxVal>
+class numeric_limits<ae::TieredInt<T, Min, MinMaxVal>> {
+ public:
+  static constexpr T lowest() { return T{0}; }
+  static constexpr T min() { return T{0}; }
+  static constexpr T max() { return ae::TieredInt<T, Min, MinMaxVal>::kUpper; }
+};
+
+template <typename T, typename Min, Min MinMaxVal>
+struct hash<ae::TieredInt<T, Min, MinMaxVal>> {
+  std::size_t operator()(ae::TieredInt<T, Min, MinMaxVal> const &packed) const {
+    return static_cast<std::size_t>(static_cast<T>(packed));
+  }
+};
+}  // namespace std
+
+#endif  // NUMERIC_TIERED_INT_H_ */
